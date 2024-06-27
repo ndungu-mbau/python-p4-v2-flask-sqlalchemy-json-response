2f4e325c5ae7281a23b9379984a4ea151d51334a
Nelson Mbau <mbau.ndungu@gmail.com>
2024-06-27 11:39:11 +0300

    feat: add view methods to return json

--- server/app.py
+++ server/app.py
@@ -1,13 +1,15 @@
 # server/app.py
 #!/usr/bin/env python3
-
+import os
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
 from models import db, Pet
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db_path = os.path.join(os.path.dirname(__file__), 'instance', 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
@@ -16,17 +18,45 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return make_response(
-        '<h1>Welcome to the pet directory!</h1>',
-        200
-    )
+    res = {'message': 'Welcome to the pet directory!'}
+    return make_response(res, 200)
 
 
 @app.route('/demo_json')
 def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
+    pet_json = {"id": 1, "name" : "Fido", "species" : "Dog"}
     return make_response(pet_json, 200)
 
+
+@app.route('/pets/<int:id>')
+def get_pet(id):
+    pet = Pet.query.get(id)
+    if pet:
+        res = {
+            'id': pet.id,
+            'name': pet.name,
+            'species': pet.species
+        }
+        status = 200
+    else:
+        res = { 'message': f'Pet with id {id} not found' }
+        status = 404
+
+    return make_response(res, status)
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = []  # array to store a dictionary for each pet
+    for pet in Pet.query.filter_by(species=species).all():
+        pet_dict = {'id': pet.id,
+                    'name': pet.name,
+                    'species': pet.species
+                    }
+        pets.append(pet_dict)
+    body = {'count': len(pets),
+            'pets': pets
+            }
+    return make_response(body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
